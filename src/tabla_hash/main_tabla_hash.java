d7561e5e6ebaab1dff8f9cc5222b7ff2d1985d9c
DiegoBúrbano <dburbanog1@miumg.edu,gt>
2022-05-01 16:32:48 -0600

    tabla_hash

---
+++ src/tabla_hash/main_tabla_hash.java
@@ -0,0 +1,19 @@
+package tabla_hash;
+import java.util.HashMap;
+
+/**
+ *
+ * @author Diego
+ */
+public class main_tabla_hash {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        frm_tabla frm = new frm_tabla();
+        frm.show();
+    }
+    
+}
